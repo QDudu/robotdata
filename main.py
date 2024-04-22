0a2ded01c54fe48b01baefc0e4c2066274fdeca7
QDudu <164342784+QDudu@users.noreply.github.com>
2024-04-23 00:33:17 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from naoqi import ALProxy
+import cv2
+
+IP="127.0.0.1"
+Port=9559
+motionProxy=ALProxy("ALMotion",IP,Port)
---
+++ pose.py
@@ -0,0 +1,30 @@
+# -*- encoding: UTF-8 -*-
+
+''' PoseInit: Small example to make Nao go to an initial position. '''
+
+import argparse
+from naoqi import ALProxy
+
+def main(robotIP, PORT=9559):
+
+    motionProxy  = ALProxy("ALMotion", robotIP, PORT)
+    postureProxy = ALProxy("ALRobotPosture", robotIP, PORT)
+
+    # Wake up robot
+    motionProxy.wakeUp()
+
+    # Send robot to Stand Init
+    postureProxy.goToPosture("StandInit", 0.5)
+
+    # Go to rest position
+    motionProxy.rest()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="127.0.0.1",
+                        help="Robot ip address")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Robot port number")
+
+    args = parser.parse_args()
+    main(args.ip, args.port)
