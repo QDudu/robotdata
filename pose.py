042c411fe8fb11ef33e7373195980e39f58b8bb1
QDudu <164342784+QDudu@users.noreply.github.com>
2024-04-23 13:19:16 +0800

    Update pose.py

--- pose.py
+++ pose.py
@@ -1,22 +1,21 @@
 # -*- encoding: UTF-8 -*-
 
-''' PoseInit: Small example to make Nao go to an initial position. '''
-
 import argparse
 from naoqi import ALProxy
 
 def main(robotIP, PORT=9559):
-
+    # 动作
     motionProxy  = ALProxy("ALMotion", robotIP, PORT)
+    # 可以调用已经定义好的姿势
     postureProxy = ALProxy("ALRobotPosture", robotIP, PORT)
 
-    # Wake up robot
+    # 调用 醒来 动作
     motionProxy.wakeUp()
 
-    # Send robot to Stand Init
+    # 控制机器人目标姿势为站立初始姿势
     postureProxy.goToPosture("StandInit", 0.5)
 
-    # Go to rest position
+    # 调用 休息 动作
     motionProxy.rest()
 
 if __name__ == "__main__":
